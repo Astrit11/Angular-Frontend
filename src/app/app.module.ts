9ea83e9359886001406503586f35892fb8c4239f
astrit11 <astritmaliku@yahoo.com>
2020-06-24 12:29:25 +0200

    Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+
+
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserAddComponent } from './user-add/user-add.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserAddComponent,
+    UserEditComponent,
+    UserDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	FormsModule,
+	AppRoutingModule,
+  HttpClientModule,
+  MatListModule,
+  MatGridListModule,
+MatDividerModule,
+MatFormFieldModule,
+MatInputModule,
+MatButtonModule,
+MatTableModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
